33d6769f6ced3fafd30d3bce809105e1c359020d
Jonathan Snook <jonathan@snook.ca>
2019-01-05 21:43:01 -0500

    initial switch to new layout

--- .eleventy.js
+++ .eleventy.js
@@ -33,7 +33,7 @@ module.exports = function(eleventyConfig) {
     });
   });
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+  // eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
